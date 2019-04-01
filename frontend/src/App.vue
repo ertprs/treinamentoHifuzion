6556a8eaa27ccc53e8ff5954735ce8524681e872
William Galleti <william.galleti@gmail.com>
2019-04-01 16:03:19 -0400

    add frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app id="inspire">
+    <hfz-menu titulo="HIFuzion WEB" :menus="menus"/>
+    <v-content>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [
+        { para: "cliente", icone: "supervised_user_circle" },
+        { para: "conta", icone: "featured_play_list" },
+        { para: "usuario", icone: "account_circle" }
+      ]
+    };
+  }
+};
+</script>
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import Toolbar from './Toolbar'
+
+Vue.component('hfz-menu', Toolbar)
