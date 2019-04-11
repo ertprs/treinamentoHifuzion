a3cde04d12a82dfdaf7198c591314b035e0b35db
William Galleti <william.galleti@gmail.com>
2019-04-11 16:29:49 -0400

    Remove test debugger url

--- docapp/api/docsapp/integracao/views.py
+++ docapp/api/docsapp/integracao/views.py
@@ -1,10 +1,1 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from docsapp.integracao.services import DocumentoIntegracao
-
-
-def qualquer(request):
-    integ = DocumentoIntegracao()
-    integ.integrar()
-    return HttpResponse('Executou', status=200)
+# views
