fd2c743f52e53eb2319765588abe7826ef9cc881
William Galleti <william.galleti@gmail.com>
2019-04-15 14:15:46 -0400

    Remove web˜

--- web/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-import Lista from './crud/Lista'
-import Form from './crud/Form'
-import Remove from './crud/Remove'
-
-Vue.component('hfz-form', Form)
-Vue.component('hfz-lista', Lista)
-Vue.component('hfz-remove', Remove)
--- web/src/views/PlanoConta.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Plano de Conta</h1>
-</template>
