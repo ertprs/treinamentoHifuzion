ca308a467b10b49f9cac82333aaa2244484edaa6
William Galleti <william.galleti@gmail.com>
2019-04-25 16:03:29 -0400

    Start project and add vuetify

--- projeto_final/web/src/main.js
+++ projeto_final/web/src/main.js
@@ -1,9 +1,12 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import './plugins/element.js'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
